7c2084116bf0674645a112618825ea53b27e0240
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2022-03-08 06:44:56 +0300

    Added URL Patterns to the blog application

---
+++ blog_site/blog_site/blog/urls.py
@@ -0,0 +1,9 @@
+# URL Patterns go here
+
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+]
--- blog_site/blog_site/blog/views.py
+++ blog_site/blog_site/blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on') #the dash before created_on makes sure that the latest post appears first and that it will be at the top of the list
     template_name = 'index.html'
 
 class PostDetail(generic.DetailView):
