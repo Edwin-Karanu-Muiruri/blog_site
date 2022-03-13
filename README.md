aad308bb2973418911d57e2806f486bec6ecb68d
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2022-03-14 01:05:22 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+## Django Blogger App
+
+A learning app meant to teach students about CRUD operations and the MVC structure in Python Django
+
+## Requirements
+Django, Python, HTML,
+
+## How to Use
+Go to 
+To add blog and view CRUD operations, press the 'Add Article' button
--- blog_site/blog_site/blog_site/settings.py
+++ blog_site/blog_site/blog_site/settings.py
@@ -79,8 +79,16 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', "localhost"),
+        'PORT': os.environ.get('DB_PORT','localhost'),
+        'TEST': {
+            'NAME': 'test_blog_site'
+        },
+
     }
 }
 
