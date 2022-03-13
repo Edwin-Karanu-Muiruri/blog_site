fabaac3cedea35deae0599d7ed5153ec37675f00
Edwin-Karanu-Muiruri <60643035+Edwin-Karanu-Muiruri@users.noreply.github.com>
2022-03-13 22:31:41 +0300

    Delete blog_site/blog_site/blog directory

--- blog_site/blog_site/blog/urls.py
+++
@@ -1,9 +0,0 @@
-# URL Patterns go here
-
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-]
